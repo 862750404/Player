e844f4d8f891f704346d90f3cad3ea161d1ef755
mac <sea>
2017-11-17 19:58:25 +0800

    di er ci

--- app/src/main/java/cashbook/imooc/android/sea/com/kincrseaplayer/MainActivity.java
+++ app/src/main/java/cashbook/imooc/android/sea/com/kincrseaplayer/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "a");
         Log.d("MainActivity", "b");
+        Log.d("MainActivity", "c");
     }
 }
